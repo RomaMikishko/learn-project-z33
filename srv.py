983aa49f5b87148002581468868cb1b65475f4b0
Roman Mikishko <rmikishko@gmail.com>
2020-08-01 16:58:58 +0300

    dz

--- srv.py
+++ srv.py
@@ -2,12 +2,85 @@ import os
 import socketserver
 from http.server import SimpleHTTPRequestHandler
 
+
 PORT = int(os.getenv("PORT", 8000))
 print(PORT)
 
 
 class MyHandler(SimpleHTTPRequestHandler):
-    pass
+    def handle_root(self):
+        pass
+
+    def handle_hello(self):
+        content = f"""
+                <html>
+                <head><title>XXX</title></head>
+                <body>
+                <h1>Hello world!</h1>
+                <p>PATH: {self.path}</p>
+                </body>
+                </html>
+                """
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-length", str(len(content)))
+        self.end_headers()
+        self.wfile.write(content.encode())
+
+    def handle_404(self):
+        msg = """NOT FOUND!!!!!!!"""
+
+        
+
+        self.send_response(404)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-length", str(len(msg)))
+        self.end_headers()
+        self.wfile.write(msg.encode())
+
+    def respond(self, code):
+        self.send_response(code)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-length", str(len(msg)))
+        self.end_headers()
+        self.wfile.write(msg.encode())
+
+    def do_GET(self):
+        path = self.build_path()
+
+        if self.path == "/":
+            self.handle_root()
+        elif self.path == "/hello/":
+            self.handle_hello()
+        else:
+            self.handle_404()
+
+    def build_path(self) -> str:
+        result = self.path
+
+        if result[-1] != "/":
+            result = f"{result}/"
+
+        return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
