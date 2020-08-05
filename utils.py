6b076cba853ebe1429bc977fa174407856115452
Roman Mikishko <rmikishko@gmail.com>
2020-08-05 21:21:52 +0300

    123

--- utils.py
+++
@@ -1,7 +0,0 @@
-def normalize_path(path: str) → str:
-   normalize_path = normalize_path
-
-   if normalize_path[-1] = "/":
-       normalize_path = f"{normalized_path}/"
-
-    return normalize_path
