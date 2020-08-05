f25d53a9d2dbfe4e711295d5dde6a9196f5bdac0
Roman Mikishko <rmikishko@gmail.com>
2020-08-05 18:39:05 +0300

    added settings and new code

--- srv.py
+++ srv.py
@@ -1,58 +1,47 @@
-import os
-import socketserver
 from http.server import SimpleHTTPRequestHandler
 
-
-PORT = int(os.getenv("PORT", 8000))
-print(PORT)
+import settings
 
 
-class MyHandler(SimpleHTTPRequestHandler):
-    def handle_root(self):
-        pass
-
-    def handle_hello(self):
-        content = f"""
-                <html>
-                <head><title>XXX</title></head>
-                <body>
-                <h1>Hello world!</h1>
-                <p>PATH: {self.path}</p>
-                </body>
-                </html>
-                """
-
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.send_header("Content-length", str(len(content)))
-        self.end_headers()
-        self.wfile.write(content.encode())
-
-    def handle_404(self):
-        msg = """NOT FOUND!!!!!!!"""
-        self.send_response(404)
-        self.send_header("Content-type", "text/plain")
-        self.send_header("Content-length", str(len(msg)))
-        self.end_headers()
-        self.wfile.write(msg.encode())
-
-    def respond(self, code):
-        msg = """NOT FOUND!!!!!!!"""
-        self.send_response(code)
-        self.send_header("Content-type", "text/plain")
-        self.send_header("Content-length", str(len(msg)))
-        self.end_headers()
-        self.wfile.write(msg.encode())
-
+class MyHttp(SimpleHTTPRequestHandler):
     def do_GET(self):
         path = self.build_path()
 
-        if self.path == "/":
+        if path == "/":
             self.handle_root()
-        elif self.path == "/hello/":
+        elif path == "/hello/":
             self.handle_hello()
         else:
             self.handle_404()
+
+    def handle_root(self):
+        return super().do_GET()
+
+    def handle_hello(self):
+        content = f"""
+        <html>
+        <head><title>Hello Page</title></head>
+        <body>
+        <h1>Hello world!</h1>
+        <p>path: {self.path}</p>
+        </body>
+        </html>
+        """
+
+        self.respond(content)
+
+    def handle_404(self):
+        msg = """NOT FOUND!!!!!!!!"""
+
+        self.respond(msg, code=404, content_type="text/plain")
+
+    def respond(self, message, code=200, content_type="text/html"):
+        self.send_response(code)
+        self.send_header("Content-type", content_type)
+        self.send_header("Content-length", str(len(message)))
+        self.send_header("Cache-control", f"max-age={settings.CACHE_AGE}")
+        self.end_headers()
+        self.wfile.write(message.encode())
 
     def build_path(self) -> str:
         result = self.path
@@ -61,9 +50,3 @@ class MyHandler(SimpleHTTPRequestHandler):
             result = f"{result}/"
 
         return result
-
-
-if __name__ == "__main__":
-    with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
-        print("it" + " works")
-        httpd.serve_forever(poll_interval=1)
